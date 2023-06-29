b1d6ebafb22a0e86d2e26417518e8e61cedbd979
UserOlena <helen.watermore@gmail.com>
2023-06-28 22:51:31 -0700

    Use flex-box to keep footer at the bottom, change styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, { useState } from 'react';
 import {
   Navbar,
   Footer,
   AboutMe,
+  Portfolio,
+  MainSection
 } from './components';
 
 function App() {
+
+  // switch (tab) {
+  //   case 'About Me':
+  //     return <AboutMe />;
+  //   case 'Portfolio':
+  //     return <Portfolio />;
+    // case 'Contact':
+    //   return < />;
+    // case 'Resume':
+    //   return < />;
+  // }
+
   return (
-    <div>
+    <div style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    }}
+    >
       <Navbar/>
       <AboutMe/>
       <Footer/>
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.getElementById('root').style.height = '100vh';
 root.render(<App/>);
